69c911b3cfe2f412a833ba2a8a300f3dcca22b96
Zhenya Kostev <zh.kostev@gmail.com>
2013-03-17 10:52:14 +0300

    Project Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project13::Application.config.secret_key_base = '8f386a41291d6a4de663fd37d43375818c245a0c08f4989f5d51a8fc2eaf42a4e5570beb6648e39bb9bebe68aacde244f6c5e16e87289adca4de8c7ad6c5fad4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Project13::Application.config.session_store :encrypted_cookie_store, key: '_project_13_session'
